8dbfac04b4b54d92affd5989193b16c0f58acf6f
Melvin Carvalho <melvincarvalho@gmail.com>
2014-09-03 17:25:07 +0200

    log in and log out

--- markbot.js
+++ markbot.js
@@ -1,26 +1,45 @@
 // Requires
 var irc  = require('irc');
 
+// Init
+var botName = 'markbot';
+var channel = '#bitmark';
+var duration = 30000;
+var joinMessage = 'Greetings, I am markbot.  Just popping in briefly, to look around.';
+var partMessage = "So long, until next time.  I'll be back!";
+
 
 // Create the configuration
 var config = {
-	channels: ["#bitmark"],
-	server: "irc.freenode.net",
-	botName: "markbot"
+  channels: [channel],
+  server: "irc.freenode.net",
+  botName: botName,
+  autoconnect: true
 };
 
 
 // Create the bot name
 var bot = new irc.Client(config.server, config.botName, {
-	channels: config.channels
+  channels: config.channels
 });
 
+
+// Join message
+bot.addListener("join", function(channel, nick, message) {
+  bot.say(channel, joinMessage);
+});
 
 // Listen for any messages
 bot.addListener("message", function(from, to, text, message) {
-
   console.log(message);
-
 });
 
-
+// part
+setTimeout(part, duration);
+function part() {
+  bot.say(config.channels[0], partMessage);
+  bot.part(config.channels[0], partMessage, function(){ 
+    console.log("Exiting");
+    process.exit(0);
+  });
+}
